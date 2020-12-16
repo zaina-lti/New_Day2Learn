0196906116c0ffa4459a46ffd781ba25ed35aa35
zaina <zaina.ahad@lntinfotech.com>
2020-12-16 14:59:57 +0530

    bugfix commit

--- class1.cs
+++ class1.cs
@@ -1,7 +1,16 @@
-partial class Program
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
 {
-  void Print Welcome()
-  {
-  Console.WriteLine("Welcome!")
-  }
-  }
+    partial class Program
+    {
+        void Print Welcome()
+        {
+            Console.WriteLine("Welcome!")
+        }
+    }
+}
